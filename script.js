6a11889a84648515ac6bfded2cf5d30aaa75596c
Penina Wanjiru <penina.wanjiru@andela.com>
2017-01-31 21:40:23 +0300

    html canvas test

---
+++ script.js
@@ -0,0 +1,7 @@
+function inititate() {
+  var elem = document.getElementById('canvas');
+  var canvas = elem.getContext('2d');
+  canvas.strokeRect(100, 100, 120, 120);
+}
+
+addEventListener("load", initiate);
