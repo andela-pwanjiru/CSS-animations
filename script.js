9b01b9dbb21dba5650fb0c172800f8ce35d15ebf
Penina Wanjiru <penina.wanjiru@andela.com>
2017-02-02 20:58:52 +0300

    Create canvas

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function inititate() {
+function initiate() {
   var elem = document.getElementById('canvas');
   var canvas = elem.getContext('2d');
+
+  //Gradient on rectangle
+  var gradient = canvas.createLinearGradient(0, 0, 10, 100)
+  gradient.addColorStop(0.5, "#0088ff")
+  gradient.addColorStop(1, "#ff0000")
+  canvas.fillStyle = gradient;
+
+  canvas.fillRect(150, 10, 200, 100);
+
+
+  // rectangle with stroke, fill and clearx
+  canvas.fillStyle = "#0000ff";
+  canvas.strokeStyle = "#ff0000";
+
+
   canvas.strokeRect(100, 100, 120, 120);
+  canvas.fillRect(110, 110, 100, 100);
+  canvas.clearRect(130, 130, 30, 30);
+
+
 }
 
 addEventListener("load", initiate);
