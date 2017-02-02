2ea4a0cd0fcd1cd49f5e2c81b77c8135f20dc3e5
Penina Wanjiru <penina.wanjiru@andela.com>
2017-02-02 22:01:13 +0300

    Add in paths to canvas

--- script.js
+++ script.js
@@ -2,23 +2,34 @@ function initiate() {
   var elem = document.getElementById('canvas');
   var canvas = elem.getContext('2d');
 
+  // Drawing using paths
+  canvas.beginPath();
+  canvas.moveTo(200, 200);
+  canvas.lineTo(200, 400);
+  canvas.lineTo(400, 400);
+  canvas.closePath();
+  // canvas.fill();
+
+
+
   //Gradient on rectangle
-  var gradient = canvas.createLinearGradient(0, 0, 10, 100)
-  gradient.addColorStop(0.5, "#0088ff")
-  gradient.addColorStop(1, "#ff0000")
+  var gradient = canvas.createLinearGradient(0, 0, 10, 100);
+  gradient.addColorStop(0.5, "#0088ff");
+  gradient.addColorStop(1, "#ff0000");
   canvas.fillStyle = gradient;
 
-  canvas.fillRect(150, 10, 200, 100);
+  canvas.fillRect(190, 10, 100, 100);
+  canvas.fillRect(400, 10, 100, 100);
 
 
-  // rectangle with stroke, fill and clearx
-  canvas.fillStyle = "#0000ff";
-  canvas.strokeStyle = "#ff0000";
+  // // rectangle with stroke, fill and clearx
+  // canvas.fillStyle = "#0000ff";
+  // canvas.strokeStyle = "#ff0000";
 
 
-  canvas.strokeRect(100, 100, 120, 120);
-  canvas.fillRect(110, 110, 100, 100);
-  canvas.clearRect(130, 130, 30, 30);
+  // canvas.strokeRect(100, 100, 120, 120);
+  // canvas.fillRect(110, 110, 100, 100);
+  // canvas.clearRect(130, 130, 30, 30);
 
 
 }
